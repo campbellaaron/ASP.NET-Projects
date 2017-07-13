24ad7e411201f594cef0050d17bc959f527b12be
campbellaaron <jenovawitness@hotmail.com>
2017-07-13 14:41:04 -0400

    Everything is hard coded, going to add more stuff

---
+++ TowerDefense/TowerDefense/Level.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace TowerDefense
+{
+    public class Level
+    {
+        private readonly Invader[] _invaders;
+
+        public Tower[] Towers { get; set; }
+
+        public Level(Invader[] invaders)
+        {
+            _invaders = invaders;
+        }
+
+        //returns true if the player wins; otherwise it's false
+        public bool Play()
+        {
+            //Run until all invaders are neutralized or an invader reaches end of path
+            int remainingInvaders = _invaders.Length;
+
+            while (remainingInvaders > 0)
+            {
+                //Each tower has the opportunity to fire on an invader
+                foreach(Tower tower in Towers){
+                    tower.FireOnInvaders(_invaders);
+                }
+
+                //Count and move the invaders that are still active
+                remainingInvaders = 0;
+                foreach(Invader invader in _invaders)
+                {
+                    if (invader.IsActive)
+                    {
+                        invader.Move();
+
+                        if(invader.HasScored)
+                        {
+                            return false;
+                        }
+
+                        remainingInvaders++;
+                    }
+                }
+			}
+            return true;
+        }
+    }
+}
--- TowerDefense/TowerDefense/MapLocation.cs
+++ TowerDefense/TowerDefense/MapLocation.cs
@@ -9,5 +9,10 @@ namespace TowerDefense
                 throw new OutOfBoundsException(x + "," + y + " is outside the boundary of the map.");
             }
         }
+
+        public bool InRangeOf(MapLocation location, int range)
+        {
+            return DistanceTo(location) <= range;
+        }
     }
 }
